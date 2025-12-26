e3f28f02690a5cc2f8da54830bae917ce86cdcaf
amaendle <AndemWue@gmail.com>
2025-12-26 04:59:30 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = "mp3pwa-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
